99ea42674a4f06cca8af23deea216084027527a9
Ali Haider <haiderjutt2k2@gmail.com>
2025-12-14 01:27:05 +0100

    Change BASE_URL to production API endpoint

--- frontend/src/api/salesapi.js
+++ frontend/src/api/salesapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://sales-forecast-api-6cw8.onrender.com";
 
 export const getForecast = async (formData) => {
   try {
